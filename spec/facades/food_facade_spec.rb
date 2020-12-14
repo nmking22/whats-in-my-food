0fa3efd8f2ddc5d5e649bc2a1c5fa56bfa18c5c1
Nick King <nickmaxking@gmail.com>
2020-12-14 10:47:10 -0700

    Add tests for FoodFacade

---
+++ spec/facades/food_facade_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'food facade' do
+  it 'finds foods and query info' do
+    facade = FoodFacade.food_query('sweet potatoes')
+
+    expect(facade).to be_a(Hash)
+
+    expect(facade).to have_key(:total_hits)
+    expect(facade[:total_hits]).to be_an(Integer)
+
+    expect(facade).to have_key(:query)
+    expect(facade[:query]).to be_a(String)
+
+    expect(facade).to have_key(:food_poros)
+    expect(facade[:food_poros]).to be_an(Array)
+    expect(facade[:food_poros][0]).to be_a(FoodPoro)
+  end
+end
