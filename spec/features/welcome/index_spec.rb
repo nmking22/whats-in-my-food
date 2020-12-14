a431036c24f3425d0eeb693f339a36643368a92d
Nick King <nickmaxking@gmail.com>
2020-12-14 09:50:27 -0700

    Change spec to align with more descriptive text field tag

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -4,7 +4,7 @@ describe 'welcome index page' do
   it 'form directs to foods index' do
     visit root_path
 
-    fill_in :q, with: 'sweet potatoes'
+    fill_in :food_search, with: 'sweet potatoes'
     click_button 'Search'
 
     expect(current_path).to eq(foods_path)
