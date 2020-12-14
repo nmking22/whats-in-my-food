fbaad1f51919072159de6204e79b62c74550f514
Nick King <nickmaxking@gmail.com>
2020-12-14 09:51:32 -0700

    Add API consumption and parsing to index action

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,9 @@
 class FoodsController < ApplicationController
   def index
-
+    query = params[:food_search]
+    response = Faraday.get("https://api.nal.usda.gov/fdc/v1/foods/search?query=#{query}") do |f|
+      f.params[:api_key] = "#{ENV['food_key']}"
+    end
+    @foods = JSON.parse(response.body, symbolize_names: true)
   end
 end
